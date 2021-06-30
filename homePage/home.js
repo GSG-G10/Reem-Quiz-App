4af093edf3e94ae1341cf44fef6175c238af9118
reemsh2000 <sh951652@gmail.com>
2021-06-30 16:19:40 +0300

    js-home-page

--- homePage/home.js
+++ homePage/home.js
@@ -1,2 +1,19 @@
-let playerName=document.getElementById('name');
-console.log(playerName.innerText)
+
+let dataArray = [];         // Array to push new data
+const dataObject = {
+    name: selector("#name").textContent,
+    score:0  
+}
+
+/* If there is data saved already in local storage, add the new data to old data*/
+let oldData = JSON.parse(localStorage.getItem("data"));
+if((oldData !== null)){
+    oldData.push(dataObject);
+    localStorage.setItem("data", JSON.stringify(oldData))
+   
+} else{     /* If local storage is empty, Push new data to the empty array */
+    dataArray.push(dataObject)  //Push object of data to the array
+
+    /* set stringified data in local storage */
+    localStorage.setItem('data', JSON.stringify(dataArray))
+}
